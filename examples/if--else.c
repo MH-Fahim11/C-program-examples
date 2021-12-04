3cd92a95e132a9592746a2a8cba241d36e22e598
Mahmudul Hasan Fahim <95010186+MH-Fahim11@users.noreply.github.com>
2021-12-04 09:49:51 +0600

    if elseif else

---
+++ examples/if--else.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int n=0;
+
+    if(n>=0){
+        printf("The number is positive.");
+    }
+    else{
+        printf("The number is negative.");
+    }
+    return 0;
+}
