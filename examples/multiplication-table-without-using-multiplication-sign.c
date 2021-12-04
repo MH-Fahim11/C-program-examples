066e16bd41f8e113671f2457907bb85bd2fdf59f
Mahmudul Hasan Fahim <95010186+MH-Fahim11@users.noreply.github.com>
2021-12-04 16:37:29 +0600

    Create multiplication-table-without-using-multiplication-sign.c

---
+++ examples/multiplication-table-without-using-multiplication-sign.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int m = 0, n = 5, i;
+
+    for (i = 1; i <= 10; i++)
+    {
+        m=m+n;
+        printf("%d X %d = %d\n",n, i, m);
+    }
+    return 0;
+}
