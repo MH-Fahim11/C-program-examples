6b5a4ac579786338a9d7be4995f2e90f1fed8900
Mahmudul Hasan Fahim <95010186+MH-Fahim11@users.noreply.github.com>
2021-12-10 06:46:43 +0600

    Update Celsius to Kelvin.c

--- examples/Celsius to Kelvin.c
+++ examples/Celsius to Kelvin.c
@@ -1,1 +1,15 @@
 #include<stdio.h>
+
+int main()
+{
+    int c, k;
+
+    printf(" Welcome, This is a Celsius to Kelvin calculator.\n Please input a Celsius value:");
+    scanf("%d", &c);
+
+    k = c + 273;
+
+    printf(" Kelvin value is : %d \n", k);
+
+    return 0;
+}
