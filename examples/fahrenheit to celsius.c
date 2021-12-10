c6a35d72d37f07f1848d9f0e08158b4c1379b850
Mahmudul Hasan Fahim <95010186+MH-Fahim11@users.noreply.github.com>
2021-12-10 06:54:46 +0600

    Update fahrenheit to celsius.c

--- examples/fahrenheit to celsius.c
+++ examples/fahrenheit to celsius.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+int main()
+{
+    int c, f;
+
+    printf(" Welcome, This is a Fahrenheit to Celsius calculator.\n Please input a Fahrenheit value:");
+    scanf("%d", &f);
+
+    c = (f - 32) * 5/9 ;
+
+    printf(" Celsius value is : %d \n", c);
+
+    return 0;
+}
